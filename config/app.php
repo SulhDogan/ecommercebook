c454e78f5b46be1d4adafad19c7ca6aa5cc34781
SulhDogan <sulhdogan@gmail.com>
2022-04-22 05:05:12 +0300

    cfg

--- config/app.php
+++ config/app.php
@@ -54,9 +54,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://sleepy-depths-80372.herokuapp.com/'),
 
-    'asset_url' => env('ASSET_URL'),
+    'asset_url' => env('ASSET_URL','https://sleepy-depths-80372.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
